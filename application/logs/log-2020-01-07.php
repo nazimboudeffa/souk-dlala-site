36f1c6fe63d6dedb3e51c006d4cf5894785dbeff
Nazim Boudeffa <boudeffa@gmail.com>
2020-01-07 15:54:27 +0100

    Added flags

--- application/logs/log-2020-01-07.php
+++ application/logs/log-2020-01-07.php
@@ -20,3 +20,60 @@ ERROR - 2020-01-07 12:19:22 --> 404 Page Not Found: /index
 ERROR - 2020-01-07 12:20:07 --> 404 Page Not Found: /index
 ERROR - 2020-01-07 12:22:38 --> 404 Page Not Found: /index
 ERROR - 2020-01-07 12:22:57 --> 404 Page Not Found: /index
+ERROR - 2020-01-07 12:42:47 --> Query error: Table 'souk-dlala.value_store' doesn't exist - Invalid query: SELECT value FROM value_store WHERE thekey = 'outOfStock'
+ERROR - 2020-01-07 12:48:45 --> Query error: Table 'souk-dlala.value_store' doesn't exist - Invalid query: SELECT value FROM value_store WHERE thekey = 'outOfStock'
+ERROR - 2020-01-07 12:49:23 --> Query error: Table 'souk-dlala.value_store' doesn't exist - Invalid query: SELECT value FROM value_store WHERE thekey = 'outOfStock'
+ERROR - 2020-01-07 12:49:25 --> Query error: Table 'souk-dlala.value_store' doesn't exist - Invalid query: SELECT value FROM value_store WHERE thekey = 'outOfStock'
+ERROR - 2020-01-07 12:49:59 --> Query error: Table 'souk-dlala.value_store' doesn't exist - Invalid query: SELECT value FROM value_store WHERE thekey = 'outOfStock'
+ERROR - 2020-01-07 12:50:10 --> Query error: Table 'souk-dlala.value_store' doesn't exist - Invalid query: SELECT value FROM value_store WHERE thekey = 'outOfStock'
+ERROR - 2020-01-07 12:50:11 --> Query error: Table 'souk-dlala.value_store' doesn't exist - Invalid query: SELECT value FROM value_store WHERE thekey = 'outOfStock'
+ERROR - 2020-01-07 12:52:04 --> Query error: Table 'souk-dlala.value_store' doesn't exist - Invalid query: SELECT value FROM value_store WHERE thekey = 'outOfStock'
+ERROR - 2020-01-07 12:52:19 --> Query error: Table 'souk-dlala.value_store' doesn't exist - Invalid query: SELECT value FROM value_store WHERE thekey = 'outOfStock'
+ERROR - 2020-01-07 12:53:06 --> 404 Page Not Found: /index
+ERROR - 2020-01-07 12:53:30 --> 404 Page Not Found: /index
+ERROR - 2020-01-07 12:54:01 --> Could not find the language line "vendor_home_page"
+ERROR - 2020-01-07 12:54:48 --> 404 Page Not Found: /index
+ERROR - 2020-01-07 12:55:21 --> 404 Page Not Found: /index
+ERROR - 2020-01-07 12:55:28 --> Could not find the language line "vendor_home_page"
+ERROR - 2020-01-07 13:01:21 --> Could not find the language line "vendor_home_page"
+ERROR - 2020-01-07 13:01:33 --> Could not find the language line "vendor_home_page"
+ERROR - 2020-01-07 13:01:41 --> Could not find the language line "vendor_home_page"
+ERROR - 2020-01-07 13:01:54 --> 404 Page Not Found: /index
+ERROR - 2020-01-07 13:02:02 --> 404 Page Not Found: /index
+ERROR - 2020-01-07 13:02:30 --> Image Upload Error: <p>You did not select a file to upload.</p>
+ERROR - 2020-01-07 13:02:30 --> Severity: Notice --> Undefined index: shop_categorie D:\GitHub\souk-dlala-site\application\modules\vendor\models\Products_model.php 49
+ERROR - 2020-01-07 13:02:30 --> Severity: Notice --> Undefined index: brand_id D:\GitHub\souk-dlala-site\application\modules\vendor\models\Products_model.php 52
+ERROR - 2020-01-07 13:02:30 --> Query error: Column 'shop_categorie' cannot be null - Invalid query: INSERT INTO `products` (`image`, `shop_categorie`, `quantity`, `position`, `brand_id`, `folder`, `vendor_id`, `time`) VALUES ('', NULL, '', '', NULL, '1578398541', '1', 1578398550)
+ERROR - 2020-01-07 13:03:00 --> Could not find the language line "vendor_home_page"
+ERROR - 2020-01-07 13:03:34 --> 404 Page Not Found: /index
+ERROR - 2020-01-07 13:04:21 --> Language image upload error: <p>You did not select a file to upload.</p>
+ERROR - 2020-01-07 13:04:22 --> 404 Page Not Found: /index
+ERROR - 2020-01-07 13:04:28 --> 404 Page Not Found: /index
+ERROR - 2020-01-07 13:05:02 --> 404 Page Not Found: /index
+ERROR - 2020-01-07 13:05:04 --> 404 Page Not Found: /index
+ERROR - 2020-01-07 13:05:15 --> 404 Page Not Found: /index
+ERROR - 2020-01-07 13:05:25 --> 404 Page Not Found: /index
+ERROR - 2020-01-07 13:05:44 --> 404 Page Not Found: /index
+ERROR - 2020-01-07 13:05:44 --> 404 Page Not Found: /index
+ERROR - 2020-01-07 13:05:46 --> 404 Page Not Found: /index
+ERROR - 2020-01-07 13:05:46 --> 404 Page Not Found: /index
+ERROR - 2020-01-07 13:05:54 --> Could not find the language line "vendor_home_page"
+ERROR - 2020-01-07 13:05:57 --> 404 Page Not Found: /index
+ERROR - 2020-01-07 13:06:59 --> 404 Page Not Found: /index
+ERROR - 2020-01-07 13:06:59 --> 404 Page Not Found: /index
+ERROR - 2020-01-07 15:09:31 --> 404 Page Not Found: /index
+ERROR - 2020-01-07 15:09:31 --> 404 Page Not Found: /index
+ERROR - 2020-01-07 15:10:03 --> 404 Page Not Found: /index
+ERROR - 2020-01-07 15:10:03 --> 404 Page Not Found: /index
+ERROR - 2020-01-07 15:10:03 --> 404 Page Not Found: /index
+ERROR - 2020-01-07 15:10:03 --> 404 Page Not Found: /index
+ERROR - 2020-01-07 15:10:03 --> 404 Page Not Found: /index
+ERROR - 2020-01-07 15:10:04 --> 404 Page Not Found: /index
+ERROR - 2020-01-07 15:10:07 --> 404 Page Not Found: /index
+ERROR - 2020-01-07 15:10:28 --> 404 Page Not Found: /index
+ERROR - 2020-01-07 15:10:28 --> 404 Page Not Found: /index
+ERROR - 2020-01-07 15:13:45 --> 404 Page Not Found: /index
+ERROR - 2020-01-07 15:14:02 --> 404 Page Not Found: /index
+ERROR - 2020-01-07 15:14:02 --> 404 Page Not Found: /index
+ERROR - 2020-01-07 15:14:04 --> 404 Page Not Found: /index
+ERROR - 2020-01-07 15:19:02 --> 404 Page Not Found: /index
