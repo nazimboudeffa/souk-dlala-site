2567e0795d3540123827b5ce0ff3c4f341fa31e9
Nazim Boudeffa <boudeffa@gmail.com>
2020-01-07 12:33:59 +0100

    First Commit

---
+++ application/logs/log-2020-01-07.php
@@ -0,0 +1,22 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2020-01-07 12:12:36 --> 404 Page Not Found: /index
+ERROR - 2020-01-07 12:12:46 --> 404 Page Not Found: /index
+ERROR - 2020-01-07 12:13:55 --> 404 Page Not Found: /index
+ERROR - 2020-01-07 12:14:00 --> 404 Page Not Found: /index
+ERROR - 2020-01-07 12:14:07 --> 404 Page Not Found: /index
+ERROR - 2020-01-07 12:14:10 --> 404 Page Not Found: /index
+ERROR - 2020-01-07 12:14:14 --> 404 Page Not Found: /index
+ERROR - 2020-01-07 12:14:40 --> 404 Page Not Found: /index
+ERROR - 2020-01-07 12:14:42 --> 404 Page Not Found: /index
+ERROR - 2020-01-07 12:14:59 --> 404 Page Not Found: /index
+ERROR - 2020-01-07 12:16:03 --> 404 Page Not Found: /index
+ERROR - 2020-01-07 12:17:36 --> 404 Page Not Found: /index
+ERROR - 2020-01-07 12:18:44 --> 404 Page Not Found: /index
+ERROR - 2020-01-07 12:18:53 --> 404 Page Not Found: /index
+ERROR - 2020-01-07 12:19:10 --> 404 Page Not Found: /index
+ERROR - 2020-01-07 12:19:13 --> 404 Page Not Found: /index
+ERROR - 2020-01-07 12:19:22 --> 404 Page Not Found: /index
+ERROR - 2020-01-07 12:20:07 --> 404 Page Not Found: /index
+ERROR - 2020-01-07 12:22:38 --> 404 Page Not Found: /index
+ERROR - 2020-01-07 12:22:57 --> 404 Page Not Found: /index
